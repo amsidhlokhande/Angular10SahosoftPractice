414e55e31c5f5b4a39a72eabd1828c4ea1c45312
amsidhlokhande <amsidhlokhande@gmail.com>
2020-12-04 14:54:16 +0530

    Preloading Modules in Angular

--- LazyLoading/src/app/app-routing.module.ts
+++ LazyLoading/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { CompanyModule } from './company/company.module';
 import { ContactusComponent } from './contactus/contactus.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
@@ -32,7 +32,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy:PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
