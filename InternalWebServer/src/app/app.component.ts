28b007db8880862b373d0ff479a71f7e01aa73fa
amsidhlokhande <amsidhlokhande@gmail.com>
2020-12-05 12:02:11 +0530

    069- Observables map in Angular 10 Angular 10 Tutorial in Hindi

--- InternalWebServer/src/app/app.component.ts
+++ InternalWebServer/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators';
+
 import { Book } from './book';
 import { BookClientService } from './book-client.service';
 
@@ -11,13 +14,15 @@ export class AppComponent implements OnInit {
 
   title = 'Angular Internal Web Server';
 
-  //books: Observable<Book[]>;
-  book:Observable<Book>;
+  books: Observable<Book[]>;
+  //book:Observable<Book>;
+  bookNames: Observable<String[]>;
 
 
   constructor(private bookService: BookClientService) {
-    //this.books = this.loadBooks();
-    this.book = this.getBookByBookId(5999);
+    this.books = this.loadBooks();
+    //this.book = this.getBookByBookId(5999);
+    this.bookNames = this.getBookNames();
   }
   ngOnInit(): void {
 
@@ -27,9 +32,14 @@ export class AppComponent implements OnInit {
     return this.bookService.getBooks();
   }
 
-  getBookByBookId(bookId:number):Observable<Book>{
+  getBookByBookId(bookId: number): Observable<Book> {
     return this.bookService.getBookById(bookId);
   }
 
+
+  getBookNames(): Observable<String[]> {
+    return this.bookService.getBooks().pipe(map(books => books.map(book => book.name)));
+
+  }
 
 }
